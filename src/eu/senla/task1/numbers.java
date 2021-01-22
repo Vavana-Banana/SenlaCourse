cfaf83300373f0b9df9032980779ecdacfcfe063
Vavana-Banana <vladimir9vladimir9@gmail.com>
2021-01-22 22:13:33 +0300

    class numbers

--- src/eu/senla/task1/numbers.java
+++ src/eu/senla/task1/numbers.java
@@ -2,102 +2,106 @@ package eu.senla.task1;
 
 public class numbers {
     //базовые типы переменных
-    private byte a;
-    private short b;
-    private int c;
-    private long d;
-    private float e;
-    private double g;
-    private char f;
-    private boolean i;
-    private String k;
-    private String l = new String();
+    private byte aByte;
+    private short bShort;
+    private int cInt;
+    private long dLong;
+    private float eFloat;
+    private double gDouble;
+    private char fChar;
+    private boolean iBollean;
+    private String kString;
+    private String lString = new String();
+
 
     public numbers(byte a) {
         this.a = a;
     }
 
     //Геттеры и сеттеры получение и запись значений в приват переменных
-    Byte aByte;
+   //Get для получения из приватной переменной создаем методы get
 
-    public byte getA() {
-        return a;
-    }
 
-    public void setA(byte a) {
-        this.a = a;
-    }
-
-    public short getB() {
-        return b;
-    }
-
-    public void setB(short b) {
-        this.b = b;
-    }
-
-    public int getC() {
-        return c;
-    }
-
-    public void setC(int c) {
-        this.c = c;
-    }
-
-    public long getD() {
-        return d;
-    }
-
-    public void setD(long d) {
-        this.d = d;
-    }
-
-    public float getE() {
-        return e;
-    }
-
-    public void setE(float e) {
-        this.e = e;
-    }
-
-    public double getG() {
-        return g;
-    }
-
-    public void setG(double g) {
-        this.g = g;
-    }
-
-    public char getF() {
-        return f;
-    }
-
-    public void setF(char f) {
-        this.f = f;
-    }
-
-    public boolean isI() {
-        return i;
-    }
-
-    public void setI(boolean i) {
-        this.i = i;
-    }
-
-    public Byte getaByte() {
+    public byte getaByte() {
         return aByte;
     }
 
-    public void setaByte(Byte aByte) {
+    public short getbShort() {
+        return bShort;
+    }
+
+    public int getcInt() {
+        return cInt;
+    }
+
+    public long getdLong() {
+        return dLong;
+    }
+
+    public float geteFloat() {
+        return eFloat;
+    }
+
+    public double getgDouble() {
+        return gDouble;
+    }
+
+    public char getfChar() {
+        return fChar;
+    }
+
+    public boolean isiBollean() {
+        return iBollean;
+    }
+
+    public String getkString() {
+        return kString;
+    }
+
+    public String getlString() {
+        return lString;
+    }
+
+    //Set для назначения приватной переменной новое значение создаем методы Set
+
+
+    public void setaByte(byte aByte) {
         this.aByte = aByte;
-
     }
 
-    public String getK () {
-        return k;
+    public void setbShort(short bShort) {
+        this.bShort = bShort;
     }
 
-    public void setK (String k){
-        this.k = k;
+    public void setcInt(int cInt) {
+        this.cInt = cInt;
+    }
+
+    public void setdLong(long dLong) {
+        this.dLong = dLong;
+    }
+
+    public void seteFloat(float eFloat) {
+        this.eFloat = eFloat;
+    }
+
+    public void setgDouble(double gDouble) {
+        this.gDouble = gDouble;
+    }
+
+    public void setfChar(char fChar) {
+        this.fChar = fChar;
+    }
+
+    public void setiBollean(boolean iBollean) {
+        this.iBollean = iBollean;
+    }
+
+    public void setkString(String kString) {
+        this.kString = kString;
+    }
+
+    public void setlString(String lString) {
+        this.lString = lString;
     }
 }
